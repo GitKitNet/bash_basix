6c563a8d5383fdcd8e2c765f2e794b35769f301d
GitKitNet <101803117+GitKitNet@users.noreply.github.com>
2022-10-09 19:33:52 +0300

    Update mydir.sh

--- mydir.sh
+++ mydir.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
-
-if [ -d "/tmp" ]; then
+DIR="/tmp"
+if [ -d "$DIR" ]; then
   echo "Directory exists"
+elif [ -f "$DIR" ]; then
+  echo "This is not Directory"
 else
   echo "Directory does not exist"
 fi
